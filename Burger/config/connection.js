dce6ed2ae702e78ae733d2cba440330a2872be53
Madel Liquido <madel.liquido@gmail.com>
2020-03-23 21:03:40 -0400

    Trying to push to Heroku

--- Burger/config/connection.js
+++ Burger/config/connection.js
@@ -1,14 +1,18 @@
-var mysql = require("mysql");
+const mysql = require("mysql");
 
-var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "",
-    database: "burgers_db"
-});
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        user: "root",
+        password: "",
+        database: "burgers_db"
+    });
+}
 
-connection.connect(function(err) {
+
+connection.connect((err) => {
     if (err) {
         console.error("error connecting: " + err.stack);
         return;
@@ -16,4 +20,5 @@ connection.connect(function(err) {
     console.log("connected as id " + connection.threadId);
 });
 
+connection.connect();
 module.exports = connection;
