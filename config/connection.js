184bd1e793fbbccdb45b7639e7ce9d6dc4bacd61
Madel Liquido <madel.liquido@gmail.com>
2020-03-24 23:05:26 -0400

    Testing deployment to Heroku

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,6 @@
-const mysql = require("mysql");
+var mysql = require('mysql');
 
-const connection;
+var connection;
 
 if (process.env.JAWSDB_URL) {
     connection = mysql.createConnection(process.env.JAWSDB_URL);
@@ -23,5 +23,5 @@ connection.connect((err) => {
     console.log("connected as id " + connection.threadId);
 });
 
-connection.connect();
+
 module.exports = connection;
